308aaabf0157d396c4bde19e048a11e23cfe8250
Valentin Biciu <valibiciu@gmail.com>
2021-11-02 15:09:49 +0200

    Favicon + Robots + fonts in header.

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,8 +25,13 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
+
+
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' },
     ],
     script: [
       { hid: 'fontawesome', src: 'https://use.fontawesome.com/468b2e542c.js', defer: true }
