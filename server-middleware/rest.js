0cc531af1dbb29c11e2a6dd74b82f100058e0a7b
Valentin Biciu <valibiciu@gmail.com>
2021-11-07 16:58:07 +0200

    Setted API_KEY in .env

--- server-middleware/rest.js
+++ server-middleware/rest.js
@@ -9,7 +9,7 @@ app.get('/categories/all', function (req, res) {
     needle('get', base_url + `/api/categories/get/all`, {
       rejectUnauthorized: false,
       json: true,
-      'X-API-KEY': 'apiKey-biciu-vali'
+      'X-API-KEY': process.env.API_KEY
     })
     .then(function(resp) {
       // console.log(resp.body);
@@ -59,7 +59,7 @@ app.get('/categories/all', function (req, res) {
     needle('get', base_url + `/api/judete/get/all`, {
       rejectUnauthorized: false,
       json: true,
-      'X-API-KEY': 'apiKey-biciu-vali'
+      'X-API-KEY': process.env.API_KEY
     })
     .then(function(resp) {
       // console.log(resp.body);
